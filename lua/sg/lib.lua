fefe1ea412421b75e7bdab8b28a07057e23cc808
TJ DeVries <devries.timothyj@gmail.com>
2023-01-19 09:38:46 -0500

    fix: add more paths to search for libs

--- lua/sg/lib.lua
+++ lua/sg/lib.lua
@@ -1,10 +1,16 @@
 -- Add the compiled version of the library to our cpath,
 -- so that we can require it
 local root_dir = vim.fn.fnamemodify(require("plenary.debug_utils").sourced_filepath(), ":p:h:h:h")
-local filepath = root_dir .. "/target/debug/?.so"
-if not string.find(package.cpath, filepath, 1, true) then
-  package.cpath = filepath .. ";" .. package.cpath
+
+local add_pattern = function(pattern)
+  local filepath = root_dir .. pattern
+  if not string.find(package.cpath, filepath, 1, true) then
+    package.cpath = filepath .. ";" .. package.cpath
+  end
 end
+
+add_pattern "/target/debug/?.so"
+add_pattern "/target/debug/?.dylib"
 
 -- Return the required libsg_nvim
 local ok, mod = pcall(require, "libsg_nvim")
