0e8af131977e971377e9b1d4bfef53efea800b38
TJ DeVries <devries.timothyj@gmail.com>
2023-06-19 15:05:42 -0400

    fixups

--- crates/sg-nvim/src/lib.rs
+++ crates/sg-nvim/src/lib.rs
@@ -1,18 +1,9 @@
 // use once_cell::sync::OnceCell;
-#![allow(unused)]
 
 use {
     mlua::{prelude::*, Function, LuaSerdeExt, SerializeOptions, Value},
-    nvim_oxi::{self as oxi, Dictionary},
-    once_cell::sync::OnceCell,
-    oxi::{libuv::AsyncHandle, print, Object},
     serde::Serialize,
     sg::{self, entry::Entry, get_access_token, get_endpoint, get_sourcegraph_version, search},
-    std::thread,
-    tokio::sync::{
-        mpsc,
-        mpsc::{UnboundedReceiver, UnboundedSender},
-    },
 };
 
 // TODO: I would like to be able to do something like this and make a constant.
@@ -26,41 +17,53 @@ where
     l.to_value_with(&t, SerializeOptions::new().serialize_none_to_null(false))
 }
 
-fn get_remote_file_contents(args: (String, String, String)) -> oxi::Result<Vec<String>> {
+#[allow(unused)]
+fn lua_print(lua: &Lua, str: &str) -> LuaResult<()> {
+    let print: Function = lua.globals().get("print")?;
+    print.call::<_, ()>(str.to_lua(lua))?;
+
+    Ok(())
+}
+
+fn get_remote_file_contents(lua: &Lua, args: (String, String, String)) -> LuaResult<LuaValue> {
     let remote = args.0;
     let hash = args.1;
     let path = args.2;
 
-    let rt = tokio::runtime::Runtime::new().to_lua_err().unwrap();
+    let rt = tokio::runtime::Runtime::new().to_lua_err()?;
     let remote_file = rt
         .block_on(async { sg::maybe_read_stuff(&remote, &hash, &path).await })
-        .to_lua_err()
-        .unwrap();
+        .to_lua_err()?;
 
-    Ok(remote_file
-        .split('\n')
-        .map(|s| s.to_string())
-        .collect::<Vec<String>>())
+    to_lua(
+        lua,
+        &remote_file
+            .split('\n')
+            .map(|s| s.to_string())
+            .collect::<Vec<String>>(),
+    )
 }
 
-// fn get_remote_directory_contents(
-//     (remote, hash, path): (String, String, String),
-// ) -> oxi::Result<LuaValue> {
-//     let rt = tokio::runtime::Runtime::new().to_lua_err()?;
-//     let directory_contents = rt
-//         .block_on(async {
-//             sg::get_remote_directory_contents(&remote, &hash, &path)
-//                 .await
-//                 .map(|v| {
-//                     v.into_iter()
-//                         .filter_map(|e| Entry::from_info(e).ok())
-//                         .collect::<Vec<_>>()
-//                 })
-//         })
-//         .to_lua_err()?;
-//
-//     directory_contents.to_lua(lua)
-// }
+fn get_remote_directory_contents(lua: &Lua, args: (String, String, String)) -> LuaResult<LuaValue> {
+    let remote = args.0;
+    let hash = args.1;
+    let path = args.2;
+
+    let rt = tokio::runtime::Runtime::new().to_lua_err()?;
+    let directory_contents = rt
+        .block_on(async {
+            sg::get_remote_directory_contents(&remote, &hash, &path)
+                .await
+                .map(|v| {
+                    v.into_iter()
+                        .filter_map(|e| Entry::from_info(e).ok())
+                        .collect::<Vec<_>>()
+                })
+        })
+        .to_lua_err()?;
+
+    directory_contents.to_lua(lua)
+}
 
 fn get_search(lua: &Lua, args: (String,)) -> LuaResult<LuaValue> {
     let path = args.0;
@@ -99,54 +102,30 @@ fn lua_get_entry(lua: &Lua, args: (String,)) -> LuaResult<LuaValue> {
     search_results.to_lua(lua)
 }
 
-type Data = i32;
-type Message = (Data, tokio::sync::oneshot::Sender<i32>);
-
-fn start_server() -> &'static UnboundedSender<Message> {
-    static CELL: OnceCell<UnboundedSender<Message>> = OnceCell::new();
-    CELL.get_or_init(|| {
-        let (sender, receiver) = mpsc::unbounded_channel::<Message>();
-        let _ = thread::spawn(|| handle_requests(receiver));
-        sender
-    })
-}
-
-fn send_request((id, method): (i32, oxi::String)) -> oxi::Result<()> {
-    print!("Received number {id} outside callback for {method:?}");
-
-    let sender = start_server();
-    let (tx, mut rx) = tokio::sync::oneshot::channel();
-    sender.send((1, tx)).expect("to send this");
-
-    let handle = AsyncHandle::new(move || {
-        let i = rx.try_recv().expect("i");
-        oxi::schedule(move |_| {
-            print!("Received number {i} from backround thread");
-            Ok(())
-        });
-        Ok::<_, oxi::Error>(())
-    })?;
-
-    // let handle = AsyncHandle::new(async move {
-    //     Ok(())
-    // });
-
-    Ok(())
-}
-
-async fn handle_requests(mut handle: UnboundedReceiver<Message>) {
-    loop {
-        if let Some((num, sender)) = handle.recv().await {
-            sender.send(num).expect("to send this stuff")
-        }
-    }
-}
-
-fn get_completions(lua: &Lua, (text, temp): (String, Option<f64>)) -> LuaResult<LuaValue> {
+fn get_info(lua: &Lua, _: ()) -> LuaResult<LuaValue> {
     let rt = tokio::runtime::Runtime::new().to_lua_err()?;
-    rt.block_on(async { sg::cody::get_completions(text, temp).await })
-        .to_lua_err()?
-        .to_lua(lua)
+
+    let tbl = lua.create_table()?;
+
+    tbl.set(
+        "sourcegraph_version",
+        match rt.block_on(async { get_sourcegraph_version().await }) {
+            Ok(version) => {
+                let version_tbl = lua.create_table()?;
+                version_tbl.set("build", version.build)?;
+                version_tbl.set("product", version.product)?;
+
+                version_tbl.to_lua(lua)?
+            }
+            Err(err) => format!("error while retrieving version: {}", err).to_lua(lua)?,
+        },
+    )?;
+
+    tbl.set("sg_nvim_version", env!("CARGO_PKG_VERSION"))?;
+    tbl.set("endpoint", get_endpoint())?;
+    tbl.set("access_token_set", get_access_token().is_ok())?;
+
+    tbl.to_lua(lua)
 }
 
 fn get_link(lua: &Lua, (bufname, line, col): (String, usize, usize)) -> LuaResult<LuaValue> {
@@ -174,70 +153,24 @@ fn get_link(lua: &Lua, (bufname, line, col): (String, usize, usize)) -> LuaResul
     .to_lua(lua)
 }
 
-#[oxi::module]
-fn libsg_nvim() -> oxi::Result<Dictionary> {
-    Ok(Dictionary::from_iter([
-        (
-            "get_remote_file_contents",
-            Object::from(oxi::Function::from_fn(get_remote_file_contents)),
-        ),
-        (
-            "send_request",
-            Object::from(oxi::Function::from_fn(send_request)),
-        ),
-    ]))
-}
+#[mlua::lua_module]
+fn libsg_nvim(lua: &Lua) -> LuaResult<LuaTable> {
+    let exports = lua.create_table()?;
 
-// fn _something(lua: &Lua) -> LuaResult<LuaTable> {
-//     let exports = lua.create_table()?;
-//
-//     exports.set(
-//         "get_remote_file_contents",
-//         lua.create_function(get_remote_file_contents)?,
-//     )?;
-//
-//     exports.set(
-//         "get_remote_directory_contents",
-//         lua.create_function(get_remote_directory_contents)?,
-//     )?;
-//
-//     exports.set("get_entry", lua.create_function(lua_get_entry)?)?;
-//     exports.set("get_search", lua.create_function(get_search)?)?;
-//     exports.set("get_info", lua.create_function(get_info)?)?;
-//     exports.set("get_link", lua.create_function(get_link)?)?;
-//
-//     exports.set("get_completions", lua.create_function(get_completions)?)?;
-//
-//     exports.set(
-//         "testing",
-//         lua.create_function(|lua, (cb,): (LuaFunction,)| {
-//             // let cb = Box::new(cb);
-//             // lua.scope(f)
-//             // thread::spawn(move || {
-//             //     let x = cb.call::<_, u32>((3, 4)).unwrap();
-//             //     println!("x: {}", x);
-//             // });
-//
-//             let x = cb.call::<_, u32>((3, 4))?;
-//             x.to_lua(lua)
-//         })?,
-//     )?;
-//
-//     // #[tokio::main]
-//     // async fn main() -> Result<()> {
-//     //     let lua = Lua::new();
-//     //     lua.globals().set("sleep", lua.create_async_function(sleep)?)?;
-//     //     let res: String = lua.load("return sleep(...)").call_async(100).await?; // Sleep 100ms
-//     //     assert_eq!(res, "done");
-//     //     Ok(())
-//     // }
-//     // tokio::spawn(async move {
-//     //     let x = sg::cody::get_completions(text, temp).await.unwrap();
-//     //     // lua.call(cb, x)
-//     //     cb.call((x,));
-//     // });
-//     //
-//     // lua.create_string("ok")?.to_lua(lua);
-//
-//     Ok(exports)
-// }
+    exports.set(
+        "get_remote_file_contents",
+        lua.create_function(get_remote_file_contents)?,
+    )?;
+
+    exports.set(
+        "get_remote_directory_contents",
+        lua.create_function(get_remote_directory_contents)?,
+    )?;
+
+    exports.set("get_entry", lua.create_function(lua_get_entry)?)?;
+    exports.set("get_search", lua.create_function(get_search)?)?;
+    exports.set("get_info", lua.create_function(get_info)?)?;
+    exports.set("get_link", lua.create_function(get_link)?)?;
+
+    Ok(exports)
+}
