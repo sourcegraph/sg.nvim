5009eb658f350e8dfc1ab317231d900e61c8569f
pegasust <pegasucksgg@gmail.com>
2023-07-25 22:56:37 -0700

    feat: add nix test

--- contrib/default.nix
+++ contrib/default.nix
@@ -1,13 +1,25 @@
 {
-  pkgs,
-  symlinkJoin,
+  pkgs ? (import <nixpkgs> {}),
+  symlinkJoin ? pkgs.symlinkJoin,
   sg-workspace ? (pkgs.callPackage (import ./workspace-drv.nix)),
   sg-plugin ? (pkgs.callPackage (import ./plugin-drv.nix)),
   meta ? (pkgs.callPackage (import ./meta.nix)),
   ...
-}:
-symlinkJoin {
-  name = "sg.nvim";
-  paths = [sg-workspace sg-plugin];
-  inherit meta;
-}
+}: let
+  sg-nvim = symlinkJoin {
+    name = "sg.nvim";
+    paths = [sg-workspace sg-plugin];
+    inherit meta;
+  };
+in
+  sg-nvim
+  // {
+    # provides quick access if we're using home-manager
+    hm-vimPlugin = {
+      plugin = sg-nvim;
+      config = ''
+        package.cpath = package.cpath .. ";${sg-nvim}/lib/*.so;${sg-nvim}/lib/*.dylib"
+      '';
+      type = "lua";
+    };
+  }
--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     pre-commit-nix.url = "github:cachix/pre-commit-hooks.nix";
     rust-overlay.url = "github:oxalica/rust-overlay";
     crane.url = "github:ipetkov/crane";
+    nixpkgs-latest-vimplugins.url = "github:nixos/nixpkgs";
   };
 
   outputs = {
@@ -19,7 +20,7 @@
 
       flake = {
         overlays.default = final: prev: {
-          sg-nvim = self.packages."${prev.system}".default;
+          inherit (self.packages.${prev.system}) sg-nvim;
         };
         # HACK: both nixpkgs.lib and pkgs.lib contain licenses
         # Technically impossible to do `callPackage` without proper `${system}`
@@ -38,6 +39,12 @@
           inherit system;
           overlays = [
             inputs.rust-overlay.overlays.default
+            (final: prev: {
+              # NOTE: use legacyPackages to prevent importing everything on latest nixpkgs
+              # we should use this because we all know teej develops against
+              # latest Plenary
+              vimPlugins = inputs.nixpkgs-latest-vimplugins.legacyPackages.${system}.vimPlugins;
+            })
           ];
         };
         toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
@@ -74,6 +81,56 @@
         };
 
         packages.default = self'.packages.all;
+        # for parallel along with our overlay
+        packages.sg-nvim = self'.packages.all;
+
+        packages.neovim-with-sg = let
+          inherit (self'.packages) sg-nvim;
+          plug = pkgs.vimPlugins;
+          cfg = pkgs.neovimUtils.makeNeovimConfig {
+            withNodeJs = true;
+            plugins = [
+              {plugin = plug.plenary-nvim;}
+              {plugin = sg-nvim;}
+            ];
+            # TODO: alternative way is to add to LUA_CPATH, correctness unknown.
+            customRC = ''
+              lua <<EOF
+              package.cpath = package.cpath .. ";${sg-nvim}/lib/*.so;${sg-nvim}/lib/*.dylib"
+              EOF
+            '';
+            # `-u` sucks, it skips a lot of things that prevents `-l` to go
+            # on happy route. We will instead use `$VIMINIT` for sandboxed neovim
+            # even in the impure shell
+            wrapRc = false;
+          };
+          vimrc-drv = pkgs.writeTextFile {
+            name = "init.vim";
+            text = cfg.neovimRcContent;
+          };
+          cfg-set-viminit =
+            cfg
+            // {
+              wrapperArgs =
+                (pkgs.lib.escapeShellArgs cfg.wrapperArgs)
+                + " "
+                + "--set VIMINIT \':source ${vimrc-drv}\'";
+            };
+        in
+          pkgs.wrapNeovimUnstable pkgs.neovim.unwrapped cfg-set-viminit;
+
+        apps.neovim-with-sg = {
+          type = "app";
+          program = self'.packages.neovim-with-sg;
+        };
+        apps.default = self'.apps.neovim-with-sg;
+
+        checks.unit-test = let
+          inherit (self'.packages) neovim-with-sg;
+        in
+          pkgs.runCommand "script-test.lua.out" {} ''
+            ${neovim-with-sg}/bin/nvim --version >$out
+          '';
 
         pre-commit = {
           settings = {
