fcf50d7a2a094219a4317e6343bbb64dece8b1d1
Petri-Johan Last <petri.last@sourcegraph.com>
2023-07-31 15:37:49 +0200

    Add floating window layout

--- lua/sg/cody/commands.lua
+++ lua/sg/cody/commands.lua
@@ -8,6 +8,7 @@ local sg = require "sg"
 local util = require "sg.utils"
 
 local CodyLayout = require "sg.components.cody_layout"
+local CodyFloatLayout = require "sg.components.cody_float_layout"
 local Message = require "sg.cody.message"
 local Speaker = require "sg.cody.speaker"
 local State = require "sg.cody.state"
@@ -45,6 +46,30 @@ end
 commands.ask = function(bufnr, start_line, end_line, message)
   local selection = vim.api.nvim_buf_get_lines(bufnr, start_line, end_line, false)
   local layout = CodyLayout.init {}
+
+  local contents = vim.tbl_flatten {
+    message,
+    "",
+    util.format_code(bufnr, selection),
+  }
+
+  layout:run(function()
+    -- context.add_context(bufnr, table.concat(selection, "\n"), layout.state)
+
+    layout.state:append(Message.init(Speaker.user, contents))
+    layout:mount()
+    layout:complete()
+  end)
+end
+
+--- Ask Cody about the selected code
+---@param bufnr number
+---@param start_line number
+---@param end_line number
+---@param message string
+commands.float = function(bufnr, start_line, end_line, message)
+  local selection = vim.api.nvim_buf_get_lines(bufnr, start_line, end_line, false)
+  local layout = CodyFloatLayout.init {}
 
   local contents = vim.tbl_flatten {
     message,
