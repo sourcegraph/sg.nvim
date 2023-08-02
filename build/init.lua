51ae582001f4c8561e8e94cc8ef2300d8c2ad619
Théo Cavignac <theo.cavignac@gmail.com>
2023-08-02 18:06:51 +0200

    fix: Detect error condition and stop waiting in build script (#63)

--- build/init.lua
+++ build/init.lua
@@ -17,7 +17,8 @@ local system = function(cmd, opts)
 
   opts.on_exit = function(_, code)
     if code ~= 0 then
-      error("failed to execute: " .. table.concat(cmd, " "))
+      status.errored = true
+      return
     end
 
     status.done = true
@@ -36,14 +37,22 @@ print "====================="
 -- or short haha. I don't know what build times are for other people
 local wait_for_status = function(status)
   vim.wait(10 * 60 * 1000, function()
-    return status.done
-  end, 10)
+    return status.done or status.errored
+  end, 200)
 end
 
 local status_workspace = system { "cargo", "build", "--workspace" }
 wait_for_status(status_workspace)
 
+if status_workspace.errored then
+  error("failed to execute build the workspace")
+end
+
 local status_bins = system { "cargo", "build", "--bins" }
 wait_for_status(status_bins)
+
+if status_bins.errored then
+  error("failed to build the binaries")
+end
 
 print "success\n"
