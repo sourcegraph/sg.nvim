7f386fd88d5592eb69669f485c4e594592fd9c04
Petri-Johan Last <petri.last@sourcegraph.com>
2023-08-15 14:53:58 +0200

    Add command comment

--- plugin/cody.lua
+++ plugin/cody.lua
@@ -30,6 +30,9 @@ vim.api.nvim_create_user_command("CodyAsk", function(command)
   cody_commands.ask(bufnr, command.line1 - 1, command.line2, command.args)
 end, { range = 2, nargs = 1 })
 
+---@command CodyCancel [[
+--- Cancels all active Cody completions.
+---@command ]]
 vim.api.nvim_create_user_command("CodyCancel", function()
   cody_commands.cancel()
 end, {})
