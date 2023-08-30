0c06c329be647019f14ef685cc98b743566230ae
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2023-08-30 13:55:21 -0500

    use env token when in github action

--- scripts/init.lua
+++ scripts/init.lua
@@ -1,7 +1,11 @@
 vim.env.SG_NVIM_TESTING = "true"
 
-vim.env.SRC_ENDPOINT = "https://sourcegraph.com"
-vim.env.SRC_ACCESS_TOKEN = "testing-token-doesnt-work"
+vim.env.SRC_ENDPOINT = "https://sourcegraph.sourcegraph.com"
+
+-- when outside of github actions set the token to a fake token
+if vim.env.GITHUB_ACTIONS ~= "true" then
+  vim.env.SRC_ACCESS_TOKEN = "testing-token-doesnt-work"
+end
 
 vim.opt.rtp:append { ".", "../plenary.nvim", "../tree-sitter-lua" }
 
