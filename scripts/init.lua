390cd244199559c24329908253e3b60a90dd7634
TJ DeVries <devries.timothyj@gmail.com>
2023-09-12 13:36:18 -0400

    fix: startup sequence force in tests

--- scripts/init.lua
+++ scripts/init.lua
@@ -7,10 +7,12 @@ vim.opt.rtp:append { ".", "../plenary.nvim", "../tree-sitter-lua" }
 
 vim.cmd [[runtime! plugin/plenary.vim]]
 vim.cmd [[runtime! plugin/sg.lua]]
-vim.cmd [[runtime! plugin/cody.lua]]
-vim.cmd [[runtime! plugin/cody-agent.lua]]
+vim.cmd [[runtime! after/plugin/sg.lua]]
+vim.cmd [[runtime! after/plugin/cody.lua]]
+vim.cmd [[runtime! after/plugin/cody-agent.lua]]
 
 require("sg").setup {
   download_binaries = false,
   accept_tos = true,
+  enable_cody = true,
 }
