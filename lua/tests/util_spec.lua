502b41e6f8c5537e64d1f0a0bc77c6dad531aa91
TJ DeVries <devries.timothyj@gmail.com>
2023-10-16 13:14:51 -0400

    fix: more lenient version parsing for node
    Closes: #162

--- lua/tests/util_spec.lua
+++ lua/tests/util_spec.lua
@@ -39,4 +39,34 @@ describe("sg.utils", function()
       eq("foo_is_cool", get_word_around_character("asdf foo_is_cool, world", 7))
     end)
   end)
+
+  describe("validate_node_output", function()
+    it("should work for just returning the string of the version", function()
+      local ok, version = utils._validate_node_output { "v18.16.0" }
+      eq(true, ok)
+      eq("18.16.0", tostring(version))
+    end)
+
+    it("should work for just returning the string of the version, empty lines", function()
+      local ok, version = utils._validate_node_output { "", "v18.16.0", "" }
+      eq(true, ok)
+      eq("18.16.0", tostring(version))
+    end)
+
+    it("should work for garbage on other lines: #162", function()
+      local ok, version = utils._validate_node_output {
+        "bash: cannot set terminal process group (-1): Inappropriate ioctl for device",
+        "bash: no job control in this shell",
+        "v20.5.1",
+      }
+
+      eq(true, ok)
+      eq("20.5.1", tostring(version))
+    end)
+
+    it("should not work for old version", function()
+      local ok, _ = utils._validate_node_output { "", "v12.16.0", "" }
+      eq(false, ok)
+    end)
+  end)
 end)
