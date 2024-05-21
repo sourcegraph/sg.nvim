03f0dca483927c3b2a9f16706c169878d374565f
TJ DeVries <devries.timothyj@gmail.com>
2024-05-21 10:17:45 -0400

    remove outdated e2e tests

--- scripts/init.lua
+++ scripts/init.lua
@@ -1,11 +1,5 @@
 vim.env.SG_NVIM_TESTING = "true"
-
 vim.env.SRC_ENDPOINT = "https://sourcegraph.sourcegraph.com"
-
--- when outside of github actions set the token to a fake token
-if vim.env.SG_NVIM_E2E ~= "true" then
-  vim.env.SRC_ACCESS_TOKEN = "testing-token-doesnt-work"
-end
 
 vim.opt.rtp:append { ".", "../plenary.nvim", "../tree-sitter-lua" }
 
