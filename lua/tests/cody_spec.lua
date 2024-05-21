b6d9774d0f2cee5e6adb4b2e5b334779aad57c92
TJ DeVries <devries.timothyj@gmail.com>
2024-05-21 10:08:41 -0400

    remove list-recipes test, it not longer is valid

--- lua/tests/cody_spec.lua
+++ lua/tests/cody_spec.lua
@@ -31,24 +31,4 @@ describe("cody", function()
     local initialized = find_initialized()
     eq(initialized, { type = "notify", method = "initialized", params = {} })
   end)
-
-  a.it("should be able to list recipes", function()
-    vim.wait(5000, find_initialized)
-
-    local err, chat_question
-    rpc.execute.list_recipes(function(err_, data)
-      err = err_
-
-      chat_question = vim.tbl_filter(function(recipe)
-        return recipe.id == "chat-question"
-      end, data)[1]
-    end)
-
-    vim.wait(1000, function()
-      return err or chat_question
-    end, 5)
-
-    eq(err, nil)
-    eq(chat_question, { id = "chat-question", title = "Chat Question" })
-  end)
 end)
