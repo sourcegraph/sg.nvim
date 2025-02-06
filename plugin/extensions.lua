158b53dbb7954e83b7478de5b1834a8b95e03827
Mahan <mahan.rahmati@protonmail.com>
2025-02-06 11:13:34 -0500

    Update extensions and add empty blink

--- plugin/extensions.lua
+++ plugin/extensions.lua
@@ -1,11 +1,19 @@
--- Only enable the extension if nvim cmp is installed
-local ok, _ = pcall(require, "cmp")
-if not ok then
-  return
+-- Check if nvim cmp is installed.
+local cmp_ok, _ = pcall(require, "cmp")
+if cmp_ok then
+  -- Set a default color for Cody
+  vim.api.nvim_set_hl(0, "CmpItemKindCody", { link = "Include", default = true })
+
+  -- Load the source, make it available to users
+  require "sg.extensions.cmp"
 end
 
--- Set a default color for Cody
-vim.api.nvim_set_hl(0, "CmpItemKindCody", { link = "Include", default = true })
+-- Check if blink cmp is installed.
+local blink_ok, _ = pcall(require, "blink.cmp")
+if blink_ok then
+  -- Set a default color for Cody
+  vim.api.nvim_set_hl(0, "BlinkItemKindCody", { link = "Include", default = true })
 
--- Load the source, make it available to users
-require "sg.extensions.cmp"
+  -- Load the source, make it available to users
+  require "sg.extensions.blink"
+end
